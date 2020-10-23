90189e2f222b772c22ee8bdd675d918bbdbe0ccf
Jorge Alvarez <jorgeuls19@gmail.com>
2020-10-23 10:37:11 -0300

    Se Empieza a crear migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,27 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->increments('id');
+            $table->string('usuario',100)->unique();
             $table->string('password');
+            $table->string('rut');
+            $table->string('nombre');
+            $table->integer('administrador')->default(0);
+            $table->integer('boleta')->default(0);
+            $table->boolean('vigente')->default(true);
+            $table->integer('boleta1')->default(0);
+            $table->integer('boleta2')->default(0);
+            $table->integer('boleta3')->default(0);
+            $table->integer('root')->default(0);
+            $table->integer('externo');
+            $table->integer('caja')->default(1);
+            $table->integer('boleta4')->default(0);
+            $table->string('email', 180)->unique();
+            $table->integer('tipo_usuario_id')->unsigned();
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('tipo_usuario_id')->references('id')->on('tipos_usuarios')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
